efe5c7a92ba5a185e6630167357b1412ad796558
Janananas <jan.gellbach@gmail.com>
2025-10-16 11:38:13 +0200

    mod script

--- src/data/00_getData.sh
+++ src/data/00_getData.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-wget https://datascientest-mlops.s3.eu-west-1.amazonaws.com/mlops_dvc_fr/raw.csv
-mv raw.csv ../../data/raw_data
+wget -P data/raw_data https://datascientest-mlops.s3.eu-west-1.amazonaws.com/mlops_dvc_fr/raw.csv
